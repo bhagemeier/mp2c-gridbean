e56bde13622141a83a23613d7d76217409511911
Bjoern Hagemeier <b.hagemeier@fz-juelich.de>
2013-02-15 11:19:05 +0100

    rename restart_ variables for clarity

--- src/main/java/eu/unicore/applications/mp2c/MP2CGridBeanParameters.java
+++ src/main/java/eu/unicore/applications/mp2c/MP2CGridBeanParameters.java
@@ -44,7 +44,7 @@ import javax.xml.namespace.QName;
  */
 public class MP2CGridBeanParameters {
 
-    static final String NS = "http://www.unicore.eu/applications/mp2c";
+	static final String NS = "http://www.unicore.eu/applications/mp2c";
 
     static final QName CONTROL_FILE_QNAME = new QName(NS, "controlFile");
     static final String CONTROL_FILE_NAME = "mp2c_ctrl.inp";
@@ -57,109 +57,110 @@ public class MP2CGridBeanParameters {
     static final QName PARALLEL_FILE_QNAME = new QName(NS, "quenchFile");
     static final String PARALLEL_FILE_NAME = "mp2c_par.inp";
 
-    // The Control file
-    static final QName CTRL_SIM_SOLUTE = new QName(NS, "simulateSolute");
-    static final QName CTRL_SIM_SOLVENT = new QName(NS, "simulateSolvent");
-    static final QName CTRL_TIMESTEPS = new QName(NS, "timesteps");
-    static final QName CTRL_TEMPERATURE = new QName(NS, "temperature");
-    static final QName CTRL_BOX_RATIO_X = new QName(NS, "box_ratio_x");
-    static final QName CTRL_BOX_RATIO_Y = new QName(NS, "box_ratio_y");
-    static final QName CTRL_BOX_RATIO_Z = new QName(NS, "box_ratio_z");
-    static final QName CTRL_BC_X = new QName(NS, "bc_x");
-    static final QName CTRL_BC_Y = new QName(NS, "bc_y");
-    static final QName CTRL_BC_Z = new QName(NS, "bc_z");
-    static final QName CTRL_SHEAR_RATE_X = new QName(NS, "sr_x");
-    static final QName CTRL_SHEAR_RATE_Y = new QName(NS, "sr_y");
-    static final QName CTRL_SHEAR_RATE_Z = new QName(NS, "sr_z");
-    static final QName CTRL_COLL_STEPS_INTERVAL = new QName(NS,
-            "coll_steps_interval");
-    static final QName CTRL_EXTERNAL_FORCE = new QName(NS, "external_force");
-    static final QName CTRL_EXTERNAL_FORCE_X = new QName(NS, "ef_x");
-    static final QName CTRL_EXTERNAL_FORCE_Y = new QName(NS, "ef_y");
-    static final QName CTRL_EXTERNAL_FORCE_Z = new QName(NS, "ef_z");
-    static final QName CTRL_RESTART_SOLUTE = new QName(NS, "restartSolute");
-    static final QName CTRL_RESTART_SOLVENT = new QName(NS, "restartSolvent");
-    static final QName CTRL_RANDOM_SEED = new QName(NS, "random_seed");
-    static final QName CTRL_LANGEVIN = new QName(NS, "langevin");
-    static final QName CTRL_LANGEVIN_GAMMA = new QName(NS, "langevin_gamma");
-    static final QName CTRL_COUPLING = new QName(NS, "coupling");
-    static final QName CTRL_MAX_SCAL = new QName(NS, "max_scal");
+	// The Control file
+	static final QName CTRL_SIM_SOLUTE = new QName(NS, "simulateSolute");
+	static final QName CTRL_SIM_SOLVENT = new QName(NS, "simulateSolvent");
+	static final QName CTRL_TIMESTEPS = new QName(NS, "timesteps");
+	static final QName CTRL_TEMPERATURE = new QName(NS, "temperature");
+	static final QName CTRL_BOX_RATIO_X = new QName(NS, "box_ratio_x");
+	static final QName CTRL_BOX_RATIO_Y = new QName(NS, "box_ratio_y");
+	static final QName CTRL_BOX_RATIO_Z = new QName(NS, "box_ratio_z");
+	static final QName CTRL_BC_X = new QName(NS, "bc_x");
+	static final QName CTRL_BC_Y = new QName(NS, "bc_y");
+	static final QName CTRL_BC_Z = new QName(NS, "bc_z");
+	static final QName CTRL_SHEAR_RATE_X = new QName(NS, "sr_x");
+	static final QName CTRL_SHEAR_RATE_Y = new QName(NS, "sr_y");
+	static final QName CTRL_SHEAR_RATE_Z = new QName(NS, "sr_z");
+	static final QName CTRL_COLL_STEPS_INTERVAL = new QName(NS,
+			"coll_steps_interval");
+	static final QName CTRL_EXTERNAL_FORCE = new QName(NS, "external_force");
+	static final QName CTRL_EXTERNAL_FORCE_X = new QName(NS, "ef_x");
+	static final QName CTRL_EXTERNAL_FORCE_Y = new QName(NS, "ef_y");
+	static final QName CTRL_EXTERNAL_FORCE_Z = new QName(NS, "ef_z");
+	static final QName CTRL_RESTART_SOLUTE = new QName(NS, "restartSolute");
+	static final QName CTRL_RESTART_SOLVENT = new QName(NS, "restartSolvent");
+	static final QName CTRL_RANDOM_SEED = new QName(NS, "random_seed");
+	static final QName CTRL_LANGEVIN = new QName(NS, "langevin");
+	static final QName CTRL_LANGEVIN_GAMMA = new QName(NS, "langevin_gamma");
+	static final QName CTRL_COUPLING = new QName(NS, "coupling");
+	static final QName CTRL_MAX_SCAL = new QName(NS, "max_scal");
 
-    // The IO file
-    static final QName IO_STANDARD_OUT = new QName(NS, "standard_out");
-    static final QName IO_STANDARD_OUT_STEPS = new QName(NS,
-            "standard_out_steps");
-    static final QName IO_STANDARD_SOLUTES = new QName(NS, "standard_solutes");
-    static final QName IO_STANDARD_SOLUTES_STEPS = new QName(NS,
-            "standard_solutes_steps");
-    static final QName IO_STANDARD_SOLVENT = new QName(NS, "standard_solvent");
-    static final QName IO_STANDARD_SOLVENT_STEPS = new QName(NS,
-            "standard_solvent_steps");
-    static final QName IO_RESTART_SOLUTE = new QName(NS, "restart_solute");
-    static final QName IO_RESTART_SOLUTES_TYPE = new QName(NS,
-            "restart_solute_type");
-    static final QName IO_RESTART_SOLUTE_STEPS = new QName(NS,
-            "restart_solute_steps");
-    static final QName IO_RESTART_SOLVENT = new QName(NS, "restart_solvent");
-    static final QName IO_RESTART_SOLVENT_TYPE = new QName(NS,
-            "restart_solvent_type");
-    static final QName IO_RESTART_SOLVENT_STEPS = new QName(NS,
-            "restart_solvent_steps");
-    static final QName IO_HISTORY_SOLUTE = new QName(NS, "history_solute");
-    static final QName IO_HISTORY_SOLUTE_TYPE = new QName(NS,
-            "history_solute_type");
-    static final QName IO_HISTORY_SOLUTE_STEPS = new QName(NS,
-            "history_solute_steps");
-    static final QName IO_HISTORY_SOLVENT = new QName(NS, "history_solvent");
-    static final QName IO_HISTORY_SOLVENT_TYPE = new QName(NS,
-            "history_solvent_type");
-    static final QName IO_HISTORY_SOLVENT_STEPS = new QName(NS,
-            "history_solvent_steps");
-    static final QName IO_XYZ_SYSTEM = new QName(NS, "xyz_system");
-    static final QName IO_XYZ_SYSTEM_STEPS = new QName(NS, "xyz_system_steps");
-    static final QName IO_XYZ_SOLUTE = new QName(NS, "xyz_solute");
-    static final QName IO_XYZ_SOLUTE_TYPE = new QName(NS, "xyz_solute_type");
-    static final QName IO_XYZ_SOLUTE_STEPS = new QName(NS, "xyz_solute_steps");
-    static final QName IO_XYZ_SOLVENT = new QName(NS, "xyz_solvent");
-    static final QName IO_XYZ_SOLVENT_TYPE = new QName(NS, "xyz_solvent_type");
-    static final QName IO_XYZ_SOLVENT_STEPS = new QName(NS, "xyz_solvent_steps");
-    static final QName IO_USER_OUTPUT = new QName(NS, "user_output");
-    static final QName IO_USER_OUTPUT_STEPS = new QName(NS, "user_output_steps");
+	// The IO file
+	static final QName IO_STANDARD_OUT = new QName(NS, "standard_out");
+	static final QName IO_STANDARD_OUT_STEPS = new QName(NS,
+			"standard_out_steps");
+	static final QName IO_STANDARD_SOLUTES = new QName(NS, "standard_solutes");
+	static final QName IO_STANDARD_SOLUTES_STEPS = new QName(NS,
+			"standard_solutes_steps");
+	static final QName IO_STANDARD_SOLVENT = new QName(NS, "standard_solvent");
+	static final QName IO_STANDARD_SOLVENT_STEPS = new QName(NS,
+			"standard_solvent_steps");
+	static final QName IO_RESTART_SOLUTE = new QName(NS, "write_restart_solute");
+	static final QName IO_RESTART_SOLUTES_TYPE = new QName(NS,
+			"restart_solute_type");
+	static final QName IO_RESTART_SOLUTE_STEPS = new QName(NS,
+			"restart_solute_steps");
+	static final QName IO_RESTART_SOLVENT = new QName(NS,
+			"write_restart_solvent");
+	static final QName IO_RESTART_SOLVENT_TYPE = new QName(NS,
+			"restart_solvent_type");
+	static final QName IO_RESTART_SOLVENT_STEPS = new QName(NS,
+			"restart_solvent_steps");
+	static final QName IO_HISTORY_SOLUTE = new QName(NS, "history_solute");
+	static final QName IO_HISTORY_SOLUTE_TYPE = new QName(NS,
+			"history_solute_type");
+	static final QName IO_HISTORY_SOLUTE_STEPS = new QName(NS,
+			"history_solute_steps");
+	static final QName IO_HISTORY_SOLVENT = new QName(NS, "history_solvent");
+	static final QName IO_HISTORY_SOLVENT_TYPE = new QName(NS,
+			"history_solvent_type");
+	static final QName IO_HISTORY_SOLVENT_STEPS = new QName(NS,
+			"history_solvent_steps");
+	static final QName IO_XYZ_SYSTEM = new QName(NS, "xyz_system");
+	static final QName IO_XYZ_SYSTEM_STEPS = new QName(NS, "xyz_system_steps");
+	static final QName IO_XYZ_SOLUTE = new QName(NS, "xyz_solute");
+	static final QName IO_XYZ_SOLUTE_TYPE = new QName(NS, "xyz_solute_type");
+	static final QName IO_XYZ_SOLUTE_STEPS = new QName(NS, "xyz_solute_steps");
+	static final QName IO_XYZ_SOLVENT = new QName(NS, "xyz_solvent");
+	static final QName IO_XYZ_SOLVENT_TYPE = new QName(NS, "xyz_solvent_type");
+	static final QName IO_XYZ_SOLVENT_STEPS = new QName(NS, "xyz_solvent_steps");
+	static final QName IO_USER_OUTPUT = new QName(NS, "user_output");
+	static final QName IO_USER_OUTPUT_STEPS = new QName(NS, "user_output_steps");
 
-    // The Solvent file
-    static final QName SOLVENT_PPC = new QName(NS, "solvent_ppc");
-    static final QName SOLVENT_PARTICLES = new QName(NS, "solvent_particles");
-    static final QName SOLVENT_MASS = new QName(NS, "solvent_mass");
-    static final QName SOLVENT_LAMBDA = new QName(NS, "solvent_lambda");
-    static final QName SOLVENT_ALPHA = new QName(NS, "solvent_alpha");
+	// The Solvent file
+	static final QName SOLVENT_PPC = new QName(NS, "solvent_ppc");
+	static final QName SOLVENT_PARTICLES = new QName(NS, "solvent_particles");
+	static final QName SOLVENT_MASS = new QName(NS, "solvent_mass");
+	static final QName SOLVENT_LAMBDA = new QName(NS, "solvent_lambda");
+	static final QName SOLVENT_ALPHA = new QName(NS, "solvent_alpha");
 
-    // The Quench file
-    static final QName QUENCH_INIT_DIM_X = new QName(NS, "quench_init_dim_x");
-    static final QName QUENCH_INIT_DIM_Y = new QName(NS, "quench_init_dim_y");
-    static final QName QUENCH_INIT_DIM_Z = new QName(NS, "quench_init_dim_z");
-    static final QName QUENCH_TGT_DIM_X = new QName(NS, "quench_tgt_dim_x");
-    static final QName QUENCH_TGT_DIM_Y = new QName(NS, "quench_tgt_dim_y");
-    static final QName QUENCH_TGT_DIM_Z = new QName(NS, "quench_tgt_dim_z");
-    static final QName QUENCH_DECR_X = new QName(NS, "decr_x");
-    static final QName QUENCH_DECR_Y = new QName(NS, "decr_y");
-    static final QName QUENCH_DECR_Z = new QName(NS, "decr_z");
+	// The Quench file
+	static final QName QUENCH_INIT_DIM_X = new QName(NS, "quench_init_dim_x");
+	static final QName QUENCH_INIT_DIM_Y = new QName(NS, "quench_init_dim_y");
+	static final QName QUENCH_INIT_DIM_Z = new QName(NS, "quench_init_dim_z");
+	static final QName QUENCH_TGT_DIM_X = new QName(NS, "quench_tgt_dim_x");
+	static final QName QUENCH_TGT_DIM_Y = new QName(NS, "quench_tgt_dim_y");
+	static final QName QUENCH_TGT_DIM_Z = new QName(NS, "quench_tgt_dim_z");
+	static final QName QUENCH_DECR_X = new QName(NS, "decr_x");
+	static final QName QUENCH_DECR_Y = new QName(NS, "decr_y");
+	static final QName QUENCH_DECR_Z = new QName(NS, "decr_z");
 
-    // The parallel file
-    static final QName PARALLEL = new QName(NS, "parallel");
+	// The parallel file
+	static final QName PARALLEL = new QName(NS, "parallel");
 
-    // Solutes
-    public static final QName SOLUTE_USUAL_LJP = new QName(NS, "usual_ljp");
-    public static final QName SOLUTE_SHIFTED_LJP = new QName(NS, "shifted_ljp");
-    public static final QName SOLUTE_RADIUS_SHIFTED_LJP = new QName(NS,
-            "radius_shifted_ljp");
-    public static final QName SOLUTE_RADIUS_SHIFTED_SHIFTED_FORCE_LJP = new QName(
-            NS, "radius_shifted_shifted_force_ljp");
-    public static final QName SOLUTE_SHIFTED_FORCE_LJP = new QName(NS,
-            "shifted_force_ljp");
-    public static final QName SOLUTE_RADIUS_SHIFTED_LJP_SHIFTED = new QName(NS,
-            "radius_shifted_ljp_shifted");
-    public static final QName CTRL_THERMOSTAT = new QName(NS, "thermostat");
-    public static QName CTRL_THERMOSTAT_COUPL = new QName(NS,
-            "thermostat_coupling");
+	// Solutes
+	public static final QName SOLUTE_USUAL_LJP = new QName(NS, "usual_ljp");
+	public static final QName SOLUTE_SHIFTED_LJP = new QName(NS, "shifted_ljp");
+	public static final QName SOLUTE_RADIUS_SHIFTED_LJP = new QName(NS,
+			"radius_shifted_ljp");
+	public static final QName SOLUTE_RADIUS_SHIFTED_SHIFTED_FORCE_LJP = new QName(
+			NS, "radius_shifted_shifted_force_ljp");
+	public static final QName SOLUTE_SHIFTED_FORCE_LJP = new QName(NS,
+			"shifted_force_ljp");
+	public static final QName SOLUTE_RADIUS_SHIFTED_LJP_SHIFTED = new QName(NS,
+			"radius_shifted_ljp_shifted");
+	public static final QName CTRL_THERMOSTAT = new QName(NS, "thermostat");
+	public static QName CTRL_THERMOSTAT_COUPL = new QName(NS,
+			"thermostat_coupling");
 
 }
